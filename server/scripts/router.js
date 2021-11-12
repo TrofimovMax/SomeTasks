86a1def21ccc76898585cc48e986dad5feed3c28
Trofimov Max <trofimovmaxdev@gmail.com>
2021-11-12 15:48:35 +0300

    add Route for DELETE request

--- server/scripts/router.js
+++ server/scripts/router.js
@@ -10,11 +10,11 @@ router.post('/todos', (req, res) => {
         const todoName = req.body.name;
         const todoDone = req.body.done;
         const todoUuid = uuidv4();
-        let todo = { name: todoName, done: todoDone, uuid: todoUuid};
+        let todo = { name: todoName, done: todoDone, uuid: todoUuid };
         let data = fs.readFileSync(filePath, "utf8");
         console.log(data);
         let todos = []
-        if(data){
+        if (data) {
             todos = JSON.parse(data);
         }
         // добавляем пользователя в массив
@@ -38,6 +38,39 @@ router.get('/todos', (req, res) => {
         const content = fs.readFileSync(filePath, "utf8");
         const todos = JSON.parse(content);
         res.status(200).send(todos);
+    } catch (e) {
+        const errObj = {
+            "code": e.code,
+            "massage": e.message,
+        }
+        res.status(500).send(errObj)
+    }
+})
+
+router.delete('/todos/:uuid', (req, res) => {
+    try {
+        const id = req.params.uuid;
+        let data = fs.readFileSync(filePath, "utf8");
+        let todos = JSON.parse(data);
+        let index = -1;
+        // находим индекс пользователя в массиве
+        for (var i = 0; i < todos.length; i++) {
+            if (todos[i].uuid == id) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            // удаляем пользователя из массива по индексу
+            const todo = todos.splice(index, 1)[0];
+            data = JSON.stringify(todos);
+            fs.writeFileSync("todos.json", data);
+            // отправляем uuid удаленного пользователя
+            res.status(200).send(todo.uuid);
+        }
+        else {
+            res.status(404).send(`error: todo with id ${req.params.uuid} does not exist`);
+        }
     } catch (e) {
         const errObj = {
             "code": e.code,
